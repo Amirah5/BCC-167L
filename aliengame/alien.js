8bd7019d9faee6b46e02c30e65a675b1073bfed5
Amirah5 <amirah.munad@gmail.com>
2022-03-23 17:44:05 -0700

    adding homework javascript1 alien game

---
+++ aliengame/alien.js
@@ -0,0 +1,36 @@
+alert("Welcome aliens to Our Website!!!");
+
+function showAlert() {
+  var endText = "Enjoy the puns 😂😂";
+  alert(endText);
+}
+
+var questionOne = prompt("What is the money called in space?");
+document.getElementById("alien-alert1").innerHTML = questionOne;
+
+function loadImage() {
+  alert("what is the money called in space?");
+}
+
+var questionone = prompt("Why did the cow want to become an astronaut?");
+document.getElementById("alien-alert2").innerHTML = questionone;
+
+var questionOne = prompt("Why did the alien go to Saturn?");
+document.getElementById("alien-alert3").innerHTML = questionOne;
+
+var questionOne = prompt("Why do aliens always spill their tea? ");
+document.getElementById("alien-alert4").innerHTML = questionOne;
+
+var questionOne = prompt("Why did Ms Moon split up with Ms Sun? ");
+document.getElementById("alien-alert5").innerHTML = questionOne;
+
+var questionOne = prompt("What does the moon put on her toast? ");
+document.getElementById("alien-alert6").innerHTML = questionOne;
+
+// let questionOne = prompt("What is the money called in
+// space?");
+//
+// if (questionOne === "bucks") {
+//   alert("starbucks");
+// }
+// document.write('<IMG SRC="images/star-bucks.jpeg"/>');
